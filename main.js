8d33f299cf290077fa69742bb543af214e973192
kriisa-studier <kristofferisaksson123@gmail.com>
2025-12-08 11:54:38 +0100

    Fixed typo

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ function createCard(data) {
   if (data.description.length > 50) {
     for (let i = 49; i >= 0; i--) {
       if (data.description.charAt(i) == " ") {
-        roomInfo.textContent = data.description.slice(0, index) + "...";
+        roomInfo.textContent = data.description.slice(0, i) + "...";
         break;
       }
     }
